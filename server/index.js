0220e1717569ba243c17064dda2749574194c0d9
rmahfuj-dev <rmahfujur782@gmail.com>
2026-01-31 16:08:17 +0600

    db connected

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
@@ -8,6 +9,27 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Server is active")
 })
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
